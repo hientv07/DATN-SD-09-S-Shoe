8356da0866e87063489e87d4aeb221176126b956
hieu26022002 <nguyenngochieu26022002@gmail.com>
2024-06-04 09:59:23 +0700

    Đẩy add thư viện pom + khung project

---
+++ Datnsd09/src/main/java/com/datnsd09/Datnsd09/Datnsd09Application.java
@@ -0,0 +1,13 @@
+package com.datnsd09.Datnsd09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Datnsd09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Datnsd09Application.class, args);
+	}
+
+}
---
+++ Datnsd09/src/test/java/com/datnsd09/Datnsd09/Datnsd09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datnsd09.Datnsd09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Datnsd09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
