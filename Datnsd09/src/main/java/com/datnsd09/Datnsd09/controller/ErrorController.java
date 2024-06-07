247354ca5c1d01ab7128753f71c23067bf62ebbd
hieu26022002 <nguyenngochieu26022002@gmail.com>
2024-06-07 16:20:48 +0700

    Đẩy thông báo lỗi error 404

---
+++ Datnsd09/src/main/java/com/datnsd09/Datnsd09/controller/ErrorController.java
@@ -0,0 +1,13 @@
+package com.datnsd09.Datnsd09.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/erorr/404")
+    public String accessDenied(){
+        return "/admin/pages-error-404/pages-error-404";
+    }
+}
