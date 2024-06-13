b702e039dc6c194c3785b1071269aab9358a397d
TranCongHieu21113 <108451353+TranCongHieu21113@users.noreply.github.com>
2024-06-13 12:48:59 +0700

    Add files via upload
    crud loai de

--- Datnsd09/src/main/java/com/datnsd09/Datnsd09/repository/LoaiDeRepository.java
+++ Datnsd09/src/main/java/com/datnsd09/Datnsd09/repository/LoaiDeRepository.java
@@ -2,8 +2,20 @@ package com.datnsd09.Datnsd09.repository;
 
 import com.datnsd09.Datnsd09.entity.LoaiDe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LoaiDeRepository extends JpaRepository<LoaiDe,Long> {
+    @Query(value = "select * from loai_de where trang_thai = 0",nativeQuery = true)
+    List<LoaiDe> fillAllDangHoatDong();
+
+    @Query(value = "select * from loai_de where trang_thai = 1",nativeQuery = true)
+    List<LoaiDe> fillAllNgungHoatDong();
+
+    @Query(value = "SELECT * FROM loai_de WHERE LOWER(ten) = LOWER(:name)", nativeQuery = true)
+    LoaiDe findDeGiayByTen(@Param("name") String name);
 }
