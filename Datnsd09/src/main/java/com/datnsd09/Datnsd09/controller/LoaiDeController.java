dfd22d0aea015c4f693627eed13b72cf2b9f5b0d
TranCongHieu21113 <108451353+TranCongHieu21113@users.noreply.github.com>
2024-06-13 12:47:08 +0700

    Add files via upload
    crud loai de

--- Datnsd09/src/main/java/com/datnsd09/Datnsd09/controller/LoaiDeController.java
+++ Datnsd09/src/main/java/com/datnsd09/Datnsd09/controller/LoaiDeController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Date;
 
@@ -20,25 +19,117 @@ public class LoaiDeController {
     @Autowired
     private LoaiDeService loaiDeService;
 
-    @GetMapping()
-    public String getAll(Model model){
-        model.addAttribute("listLoaiDe", loaiDeService.getAll());
+    @GetMapping("")
+    public String hienThi(
+            Model model
+    ) {
+//        UserInfoUserDetails name = principalCustom.getCurrentUserNameAdmin();
+//        if (name != null) {
+//            model.addAttribute("tenNhanVien", principalCustom.getCurrentUserNameAdmin().getHoVaTen());
+//        } else {
+//            return "redirect:/login";
+//        }
+        model.addAttribute("listLoaiDe", loaiDeService.findAll());
         model.addAttribute("loaiDe", new LoaiDe());
         return "/admin/loai-de/loai-de";
     }
 
     @PostMapping("/add")
-    public String add(@Valid @RequestParam("ten") String ten,
-                      @RequestParam("ngayTao") String ngayTao,
-                      @RequestParam("ngaySua") String ngaySua,
-                      @RequestParam("trangThai") String trangThai) {
-        LoaiDe loaiDe=LoaiDe.builder()
-                .ten(ten)
-                .ngayTao(new Date())
-                .ngaySua(new Date())
-                .trangThai(Integer.valueOf(trangThai))
-                .build();
-        loaiDeService.add(loaiDe);
-        return "redirect:/admin/loai-de";
+    public String add(@Valid @ModelAttribute("loaiDe") LoaiDe loaiDe,
+                      BindingResult result,
+                      Model model,
+                      RedirectAttributes redirectAttributes
+    ) {
+        if (result.hasErrors()) {
+            model.addAttribute("checkModal", "modal");
+            model.addAttribute("checkThongBao", "thaiBai");
+            model.addAttribute("listLoaiDe", loaiDeService.findAll());
+            return "/admin/loai-de/loai-de";
+        } else if (!loaiDeService.checkTenTrung(loaiDe.getTen())) {
+            model.addAttribute("checkModal", "modal");
+            model.addAttribute("checkThongBao", "thaiBai");
+            model.addAttribute("checkTenTrung", "Loại đế đã tồn tại");
+            model.addAttribute("listLoaiDe", loaiDeService.findAll());
+            return "/admin/loai-de/loai-de";
+        } else {
+            redirectAttributes.addFlashAttribute("checkThongBao", "thanhCong");
+            loaiDe.setNgayTao(new Date());
+            loaiDe.setNgaySua(new Date());
+            loaiDe.setTrangThai(0);
+            loaiDeService.save(loaiDe);
+            return "redirect:/admin/loai-de";
+        }
+
+    }
+    @GetMapping("/dang-hoat-dong")
+    public String hienThiDangHoatDong(
+            Model model
+    ) {
+//        UserInfoUserDetails name = principalCustom.getCurrentUserNameAdmin();
+//        if (name != null) {
+//            model.addAttribute("tenNhanVien", principalCustom.getCurrentUserNameAdmin().getHoVaTen());
+//        } else {
+//            return "redirect:/login";
+//        }
+        model.addAttribute("listLoaiDe", loaiDeService.getAllDangHoatDong());
+        model.addAttribute("loaiDe", new LoaiDe());
+        return "/admin/loai-de/loai-de";
+    }
+
+    @GetMapping("/ngung-hoat-dong")
+    public String hienThiNgungHoatDong(
+            Model model
+    ) {
+//        UserInfoUserDetails name = principalCustom.getCurrentUserNameAdmin();
+//        if (name != null) {
+//            model.addAttribute("tenNhanVien", principalCustom.getCurrentUserNameAdmin().getHoVaTen());
+//        } else {
+//            return "redirect:/login";
+//        }
+        model.addAttribute("listLoaiDe", loaiDeService.getAllNgungHoatDong());
+        model.addAttribute("loaiDe", new LoaiDe());
+        return "/admin/loai-de/loai-de";
+    }
+
+    @GetMapping("/view-update/{id}")
+    public String viewUpdate(
+            Model model,
+            @PathVariable("id") Long id
+    ) {
+//        UserInfoUserDetails name = principalCustom.getCurrentUserNameAdmin();
+//        if (name != null) {
+//            model.addAttribute("tenNhanVien", principalCustom.getCurrentUserNameAdmin().getHoVaTen());
+//        } else {
+//            return "redirect:/login";
+//        }
+        LoaiDe loaiDe = loaiDeService.getById(id);
+        model.addAttribute("loaiDe", loaiDe);
+        return "/admin/loai-de/sua-loai-de";
+    }
+
+    @PostMapping("/update")
+    public String update(@Valid
+                         @ModelAttribute("loaiDe") LoaiDe loaiDe,
+                         BindingResult result,
+                         Model model,
+                         RedirectAttributes redirectAttributes
+    ) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("checkThongBao", "thaiBai");
+            return "/admin/loai-de/sua-loai-de";
+        } else if (!loaiDeService.checkTenTrungSua(loaiDe.getId(), loaiDe.getTen())) {
+            model.addAttribute("checkThongBao", "thaiBai");
+            model.addAttribute("checkTenTrung", "Loại đế đã tồn tại");
+            return "/admin/loai-de/sua-loai-de";
+        } else {
+            redirectAttributes.addFlashAttribute("checkThongBao", "thanhCong");
+            LoaiDe ld = loaiDeService.getById(loaiDe.getId());
+            loaiDe.setNgayTao(ld.getNgayTao());
+            loaiDe.setNgaySua(new Date());
+            loaiDeService.update(loaiDe);
+            return "redirect:/admin/loai-de";
+        }
+
     }
 }
