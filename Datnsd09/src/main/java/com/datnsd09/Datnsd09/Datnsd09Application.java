d5d4927c547f88fcced21dc6da7cc9f83d887394
TranCongHieu21113 <108451353+TranCongHieu21113@users.noreply.github.com>
2024-07-30 21:35:30 +0700

    fix loi khong chuyen trang thai cua voucher

--- Datnsd09/src/main/java/com/datnsd09/Datnsd09/Datnsd09Application.java
+++ Datnsd09/src/main/java/com/datnsd09/Datnsd09/Datnsd09Application.java
@@ -2,8 +2,10 @@ package com.datnsd09.Datnsd09;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Datnsd09Application {
 
 	public static void main(String[] args) {
